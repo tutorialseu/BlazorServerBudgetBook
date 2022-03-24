016b163aa9d23272941047756d6eb53621c0b02d
JannickL <jannick.leismann@gmx.de>
2022-03-24 14:07:08 +0100

    Project commit

---
+++ BudgetBook/Data/BudgetBookService.cs
@@ -0,0 +1,23 @@
+﻿namespace BudgetBook.Data
+{
+    public class BudgetBookService
+    {
+
+        private static List<Entry> mockDb = new List<Entry>();
+
+
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            mockDb.Add(newEntry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+
+
+
+    }
+}
---
+++ BudgetBook/Data/Entry.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetBook.Data
+{
+    public class Entry
+    {
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
